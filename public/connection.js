26318e724f1262db518275e9112fdaae2de93cc7
FranRnS <f3248486@gmail.com>
2023-03-19 22:10:42 -0500

    frontend sin calendario

--- public/connection.js
+++ public/connection.js
@@ -3,6 +3,7 @@ function cora(){
   fetch('/coords')
     .then(response => response.json())
     .then(data => {
+      console.log("ESTA ES UNA PUREBAAAA: "+(data.time))
       document.getElementById('lat').innerText = data.lat;
       document.getElementById('long').innerText = data.long;
       document.getElementById('date').innerText = data.date;
