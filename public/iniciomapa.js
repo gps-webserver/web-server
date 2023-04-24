3a0efdddee2270ed7985a34d66e392cab5d3701b
elemirariaszambrano <elemirariaszambrano@gmail.com>
2023-04-24 14:02:28 -0500

    mapa de calor

--- public/iniciomapa.js
+++ public/iniciomapa.js
@@ -1,5 +1,5 @@
 let map = L.map('map').setView([11.019067669425738,-74.85135899187047],50)
-var heatLayer;
+var polyline;
 //Agregar tilelAyer mapa base desde openstreetmap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,7 +13,7 @@ let Icon = L.icon({
 
 let marker = L.marker([11.019067669425738,-74.85135899187047],{icon:Icon}).addTo(map);
 vector=[[11.0071,-74.8092]]
-heatLayer = L.heatLayer(vector, {radius: 25, blur: 15, maxZoom:17, }).addTo(map);
+polyline = L.polyline(vector, {color: 'purple'}).addTo(map);
 
 function updateLastLocation(lat,lng){
   //mover el marcador
@@ -21,7 +21,7 @@ function updateLastLocation(lat,lng){
   //centrar el mapa
   map.panTo([lat, lng]);
   // crear la polilinea en tiempo real
-  heatLayer.addLatLng([lat, lng, son]);
+  polyline.addLatLng([lat, lng]);
 }
 
 setInterval(() => {
@@ -29,7 +29,7 @@ setInterval(() => {
     .then(response => response.json())
     .then(data => {
       
-      updateLastLocation(data.lat, data.long, data.song);
+      updateLastLocation(data.lat, data.long);
       
     });
 }, 1001);
