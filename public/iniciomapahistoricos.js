2846681c4a51e921d133e652629148d7392c1fa5
elemirariaszambrano <elemirariaszambrano@gmail.com>
2023-05-02 08:20:04 -0500

    prueba

--- public/iniciomapahistoricos.js
+++ public/iniciomapahistoricos.js
@@ -8,7 +8,10 @@ var Datafound = document.getElementById('Datafound');
 
 //Agregar tilelAyer mapa base desde openstreetmap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 }).addTo(map);
 
 let Icon = L.icon({
