d6070d578053b14a148080be626793356d41817d
FranRnS <f3248486@gmail.com>
2023-05-16 09:28:23 -0500

    kassa

--- public/iniciomapahistoricos.js
+++ public/iniciomapahistoricos.js
@@ -29,7 +29,7 @@ const slider = document.getElementById("mySlider");
 slider.addEventListener("input", function() {
   radio = slider.value;
   if (circle) {
-    circle.setRadius(radio/100*1000);
+    circle.setRadius(radio*2);
   }
 });
 
@@ -41,7 +41,7 @@ map.on('click', function(e) {
   }
   // Crea un círculo en el centro del mapa con un radio de 500 metros
   circle = L.circle(e.latlng, {
-  radius: radio, // Radio en metros
+  radius: radio*2, // Radio en metros
   color: '#553184', // Color del borde del círculo
   fillColor: '#BEAED3', // Color de relleno del círculo
   fillOpacity: 0.5 // Opacidad del relleno del círculo
